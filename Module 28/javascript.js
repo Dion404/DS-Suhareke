e1eb3cda56e7b540d3cf3d6aea886f94343c6601
Dion404 <dionmahmutaj1@gmail.com>
2025-03-24 19:20:02 +0100

    Module 28

---
+++ Module 28/javascript.js
@@ -0,0 +1,7 @@
+var btn1 = document.getElementById('btn1');
+btn1.addEventListener('click', function () {
+    alert('hello2')
+});
+
+ourElement.addEventListener('click', function1);
+ourElement.addEventListener('click', function2);
