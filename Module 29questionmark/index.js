e010f35dde416861c54bc5d0f9f1815b4eab65de
Dion404 <dionmahmutaj1@gmail.com>
2025-03-26 19:17:10 +0100

    Module 29?

---
+++ Module 29questionmark/index.js
@@ -0,0 +1,7 @@
+btn1.onclick = function () {
+txt1.style.color="purple";
+txt1.style.backgroundColor="lightgray";
+txt1.style.textAlign="center";
+txt1.style.fontSize="200px";
+txt1.style.padding="50px"; 
+}
