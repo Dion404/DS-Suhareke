7f5bd5559b7314847b5c26e3a7cb6941c5686321
Dion404 <dionmahmutaj1@gmail.com>
2025-04-02 19:14:53 +0200

    Module 30

--- Module 30/index.js
+++ Module 30/index.js
@@ -36,3 +36,16 @@ var students =["John", "Doe"]
 var [s1,s2]=students;
 console.log(s1);
 console.log(s2);
+
+var places =["London", "Paris", "New York", "Berlin"]
+
+var [firstPlace, secondPlace] = places;
+console.log(secondPlace);
+
+var numbers = [1,2,3,4,5,6,7,8,9,10]
+
+var[first,second, ...otherNumbers]= numbers;
+
+console.log(first)
+console.log(second);
+console.log(otherNumbers.toString());
