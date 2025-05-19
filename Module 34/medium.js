617b6cc05796eb1bfc4bbca8b1039189aed27ffb
Dion404 <dionmahmutaj1@gmail.com>
2025-05-19 18:21:37 +0200

    Module 35

---
+++ Module 34/medium.js
@@ -0,0 +1,5 @@
+alert("Starting in 3 seconds...");
+setTimeout(function(){alert("3...");}, 1000);
+setTimeout(function(){alert("2...");}, 2000);
+setTimeout(function(){alert("1...");}, 3000);
+setTimeout(function(){alert("Go!");}, 4000);
